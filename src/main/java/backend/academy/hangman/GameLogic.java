2c088c35d7d86fe6e51b7322817b8a331961713f
Howepu <125370383+Howepu@users.noreply.github.com>
2024-09-09 11:22:08 +0300

    Изменён тип Set у guessedLetters. Изменена реализация метода makeGuess, соответствие паттерну раннего вывода. В метод makeGuess добавлен механизм подсказок.

--- src/main/java/backend/academy/hangman/GameLogic.java
+++ src/main/java/backend/academy/hangman/GameLogic.java
@@ -1,9 +1,9 @@
 package backend.academy.hangman;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Pattern;
 import lombok.Getter;
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class GameLogic {
 
     private static final int MAX_ATTEMPTS = 6;
+    private  static final int ATTEMPTS_TO_HELP = 3;
     private static final int HEAD = 5;
     private static final int BODY = 4;
     private static final int LEFT_ARM = 3;
@@ -32,14 +33,16 @@ public class GameLogic {
 
     @Getter private String word;
     @Getter private int attemptsLeft;
-    private Set<Character> guessedLetters;
+    @Getter private Set<String> guessedLetters;
     private List<Character> displayedChars;
+    private boolean helpUsed;
 
     public GameLogic(String word, int attemptsLeft) {
         this.word = word.toLowerCase();
         this.attemptsLeft = attemptsLeft;
         this.guessedLetters = new HashSet<>();
         this.displayedChars = new ArrayList<>();
+        this.helpUsed = false;
         for (char c : word.toCharArray()) {
             displayedChars.add('_');
         }
@@ -47,29 +50,66 @@ public class GameLogic {
 
     public void makeGuess(String guess) {
 
+        String normalizedGuess = guess.toLowerCase();
+        boolean invalidGuess = !isValidGuess(normalizedGuess);
+        boolean alreadyGuessed = guessedLetters.contains(normalizedGuess);
 
-        char letter = guess.toLowerCase().charAt(0);
-
-        if (guess.length() != 1 || guessedLetters.contains(letter) || !Pattern.matches("[а-яА-Я]", guess)) {
-            log.info("Введите букву корректно!");
+        if (invalidGuess || alreadyGuessed) {
+            if (invalidGuess) {
+                log.info("Введите букву корректно!");
+            } else {
+                log.info("Эту букву уже гадали!");
+            }
             return;
         }
 
-        guessedLetters.add(letter);
+        guessedLetters.add(normalizedGuess);
 
-        if (word.contains(String.valueOf(letter))) {
+        if (word.contains(normalizedGuess)) {
             log.info("Правильно!");
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == letter) {
-                    displayedChars.set(i, letter);
-                }
-            }
+            updateDisplayedChars(normalizedGuess.charAt(0));
         } else {
             log.info("Неправильно!");
             attemptsLeft--;
         }
+
+        askForHelp();
+
     }
 
+
+    private boolean isValidGuess(String guess) {
+        return !(guess.isEmpty() || guess.length() != 1 || !Pattern.matches("[а-яА-Я]", guess));
+    }
+
+    private void updateDisplayedChars(char letter) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                displayedChars.set(i, letter);
+            }
+        }
+    }
+
+    private boolean shouldOfferHelp() {
+        return attemptsLeft <= ATTEMPTS_TO_HELP && !helpUsed && attemptsLeft != 0;
+    }
+
+
+
+    public void askForHelp() {
+        if (shouldOfferHelp()) {
+            log.info("Хотите получить подсказку? (да/нет)");
+            Scanner scanner = new Scanner(System.in);
+            String response = scanner.nextLine().trim().toLowerCase();
+            if ("да".equals(response)) {
+                String hint = HelpProvider.getHelp(word);
+                log.info("Подсказка: " + hint);
+                helpUsed = true;
+            } else if ("нет".equals(response) || "".equals(response)) {
+                log.info("Подсказка не была использована.");
+            }
+        }
+    }
 
     public String getDisplayedChars() {
         StringBuilder displayedWordBuilder = new StringBuilder();
@@ -80,12 +120,13 @@ public class GameLogic {
     }
 
     public boolean isGameOver() {
-        return attemptsLeft == 0 || displayedChars.equals(new HashSet<>(Collections.singleton(word.toCharArray())));
+        return attemptsLeft == 0 || !displayedChars.contains('_');
     }
 
     public boolean isWon() {
-        return displayedChars.equals(new HashSet<>(Collections.singleton(word.toCharArray())));
+        return !displayedChars.contains('_');
     }
+
 
 
 
